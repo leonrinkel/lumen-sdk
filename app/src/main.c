c28e855d2ff629ceb9477f76fbb3acb9909f847a
Leon Rinkel <leon@rinkel.me>
2023-10-27 16:46:09 +0200

    Add gamma correction, write without response

--- app/src/main.c
+++ app/src/main.c
@@ -35,6 +35,42 @@ static struct bt_uuid_128 lumen_rgb_uuid = BT_UUID_INIT_128(
 #define RGB_MAX_LEN 3
 static uint8_t rgb_value[RGB_MAX_LEN] = {0};
 
+static const uint8_t gamma_correction[] =
+{
+	  0,   0,   0,   0,   0,   0,   0,   0,
+	  0,   0,   0,   0,   0,   0,   0,   0,
+	  0,   0,   0,   0,   0,   0,   0,   0,
+	  0,   0,   0,   0,   1,   1,   1,   1,
+	  1,   1,   1,   1,   1,   1,   1,   1,
+	  1,   2,   2,   2,   2,   2,   2,   2,
+	  2,   3,   3,   3,   3,   3,   3,   3,
+	  4,   4,   4,   4,   4,   5,   5,   5,
+	  5,   6,   6,   6,   6,   7,   7,   7,
+	  7,   8,   8,   8,   9,   9,   9,  10,
+	 10,  10,  11,  11,  11,  12,  12,  13,
+	 13,  13,  14,  14,  15,  15,  16,  16,
+	 17,  17,  18,  18,  19,  19,  20,  20,
+	 21,  21,  22,  22,  23,  24,  24,  25,
+	 25,  26,  27,  27,  28,  29,  29,  30,
+	 31,  32,  32,  33,  34,  35,  35,  36,
+	 37,  38,  39,  39,  40,  41,  42,  43,
+	 44,  45,  46,  47,  48,  49,  50,  50,
+	 51,  52,  54,  55,  56,  57,  58,  59,
+	 60,  61,  62,  63,  64,  66,  67,  68,
+	 69,  70,  72,  73,  74,  75,  77,  78,
+	 79,  81,  82,  83,  85,  86,  87,  89,
+	 90,  92,  93,  95,  96,  98,  99, 101,
+	102, 104, 105, 107, 109, 110, 112, 114,
+	115, 117, 119, 120, 122, 124, 126, 127,
+	129, 131, 133, 135, 137, 138, 140, 142,
+	144, 146, 148, 150, 152, 154, 156, 158,
+	160, 162, 164, 167, 169, 171, 173, 175,
+	177, 180, 182, 184, 186, 189, 191, 193,
+	196, 198, 200, 203, 205, 208, 210, 213,
+	215, 218, 220, 223, 225, 228, 231, 233,
+	236, 239, 241, 244, 247, 249, 252, 255
+};
+
 static ssize_t read_rgb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
 	void* buf, uint16_t len, uint16_t offset)
 {
@@ -68,9 +104,9 @@ static ssize_t write_rgb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
 
 	for (int i = 0; i < STRIP_NUM_PIXELS; i++)
 	{
-		pixels[i].r = value[0];
-		pixels[i].g = value[1];
-		pixels[i].b = value[2];
+		pixels[i].r = gamma_correction[value[0]];
+		pixels[i].g = gamma_correction[value[1]];
+		pixels[i].b = gamma_correction[value[2]];
 	}
 
 	return len;
@@ -79,7 +115,8 @@ static ssize_t write_rgb(struct bt_conn* conn, const struct bt_gatt_attr* attr,
 BT_GATT_SERVICE_DEFINE(lumen_svc,
 	BT_GATT_PRIMARY_SERVICE(&lumen_uuid),
 	BT_GATT_CHARACTERISTIC(&lumen_rgb_uuid.uuid,
-		BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE | BT_GATT_CHRC_INDICATE,
+		BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE |
+			BT_GATT_CHRC_WRITE_WITHOUT_RESP | BT_GATT_CHRC_INDICATE,
 		BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
 		read_rgb, write_rgb, rgb_value
 	),
